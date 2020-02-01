e22520954be6f2991c18d9b2efc78a99b8a92b0a
Mark <markzero27@gmail.com>
2020-02-02 01:43:03 +0800

    Elder detials ready for integration

--- src/app/layout/elders-details/elders-details.component.ts
+++ src/app/layout/elders-details/elders-details.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EldersDetailsComponent implements OnInit {
   tab = 1;
+  updating1 = false;
+  updating2 = false;
+  updating3 = false;
+  updating4 = false;
+
   constructor() { }
 
   ngOnInit() {
